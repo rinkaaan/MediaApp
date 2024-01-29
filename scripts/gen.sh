67dfa37eb7551d507c7501587360a9657f4801e4
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2024-01-28 22:44:13 -0800

    Init

---
+++ scripts/gen.sh
@@ -0,0 +1,17 @@
+WORKPLACE="$HOME/workplace/Media"
+
+WORKSPACE="$WORKPLACE/MediaApi"
+
+(
+  cd "$WORKSPACE"
+  ./scripts/gen.sh
+)
+
+WORKSPACE="$WORKPLACE/MediaApp"
+SCHEMA_PATH="$WORKPLACE/MediaApi/api/openapi.yaml"
+
+(
+  cd "$WORKSPACE"
+  rm -rf openapi-client
+  npx openapi -i "$SCHEMA_PATH" -o openapi-client
+)
